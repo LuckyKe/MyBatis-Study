f367fb92fbbdecfb265e8de241d25105f1c4908d
rediske <854706195@qq.com>
2022-07-17 17:42:54 +0800

    Map传递参数和模糊查询

--- mybatis-01/src/test/java/com/keke/dao/UserMapperTest.java
+++ mybatis-01/src/test/java/com/keke/dao/UserMapperTest.java
@@ -5,9 +5,25 @@ import com.keke.utils.MybatisUtils;
 import org.apache.ibatis.session.SqlSession;
 import org.junit.Test;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class UserMapperTest {
+    @Test
+    public void getUserLike() {
+        SqlSession sqlSession = MybatisUtils.getSqlSession();
+        UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+
+        List<User> userList = userMapper.getUserLike("%李%");
+
+        for (User user : userList) {
+            System.out.println(user);
+        }
+
+        sqlSession.close();
+    }
+
     @Test
     public void test() {
         // 1.获取sqlSession对象
@@ -40,6 +56,21 @@ public class UserMapperTest {
         sqlSession.close();
     }
 
+    @Test
+    public void getUserById2() {
+        SqlSession sqlSession = MybatisUtils.getSqlSession();
+
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+        Map<String, Object> map = new HashMap<>();
+
+        map.put("helloId", 1);
+
+        User user = mapper.getUserById2(map);
+        System.out.println(user);
+
+        sqlSession.close();
+    }
+
     // 增删改需要提交事务
     @Test
     public void addUser() {
@@ -53,6 +84,20 @@ public class UserMapperTest {
         }
 
         // 提交事务
+        sqlSession.commit();
+        sqlSession.close();
+    }
+
+    @Test
+    public void addUser2() {
+        SqlSession sqlSession = MybatisUtils.getSqlSession();
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put("userid", 5);
+        map.put("userName", "Hello");
+        map.put("password", "2222333");
+        mapper.addUser2(map);
+
         sqlSession.commit();
         sqlSession.close();
     }
